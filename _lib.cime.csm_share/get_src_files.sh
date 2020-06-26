b7e8bc9fe593d29b5b5ef24bea0578db8758ded2
kvrigor <kvrigor@users.noreply.github.com>
2020-06-26 11:23:32 +0000

    Checked in progess for CSM share. Build fails due to missing MCT library so I will have to do that first.

--- _lib.cime.csm_share/get_src_files.sh
+++ _lib.cime.csm_share/get_src_files.sh
@@ -21,11 +21,23 @@ copy_csm_share () {
     cp ../cime/src/share/RandNum/include/* src/include
 
     # Remove unnecessary files
-    find src -type f -iname CMakeLists.txt
+    find src -type f -iname CMakeLists.txt -delete
     rm -rf src/share/esmf_wrf_timemgr/unittests
 
+    # Convert .in files to valid Fortran files
+    genf90=`realpath ../cime/src/externals/genf90/genf90.pl`
+    (
+    cd src/share/util
+    $genf90 shr_assert_mod.F90.in > shr_assert_mod.F90
+    $genf90 shr_frz_mod.F90.in > shr_frz_mod.F90
+    $genf90 shr_infnan_mod.F90.in > shr_infnan_mod.F90
+    # rm shr_assert_mod.F90.in
+    # rm shr_frz_mod.F90.in
+    # rm shr_infnan_mod.F90.in
+    )
+
     # Generate file list 
-    find src \( -iname "*.F90" -or -iname "*.c" \) | sort > src/srcFiles.txt
+    find src \( -iname "*.F90" -or -iname "*.c" \) | sed -e "s|^src/||" > src/srcFiles.txt
     find src -iname "*.in" | sort > src/inFiles.txt
     echo "Number of source files: $(wc -l src/srcFiles.txt)"
     tree src -dL 4
--- _lib.cime.csm_share/install.sh
+++ _lib.cime.csm_share/install.sh
@@ -5,6 +5,6 @@ if [ ! -d src ]; then
     copy_csm_share
 fi
 cp -v CMakeLists.txt src/
-# cmake -S src -B bld -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpifort -DCMAKE_INSTALL_PREFIX=bld
-# cmake --build bld
-# cmake --install bld
+cmake -S src -B bld -DCMAKE_C_COMPILER=mpicc -DCMAKE_Fortran_COMPILER=mpif90 -DCMAKE_INSTALL_PREFIX=bld
+cmake --build bld
+cmake --install bld
