81b028384b024d2599e82d62cc314c5c5b1a541f
kvrigor <kvrigor@users.noreply.github.com>
2020-07-21 15:10:03 +0200

    Get build path from env via CESM_BUILD_PATH

--- bld_cmake/install.sh
+++ bld_cmake/install.sh
@@ -1,26 +1,19 @@
 #!/usr/bin/env bash
-
 set -Eeo pipefail
 
 if [ ! -d src ]; then
     ./get_src_files.sh
 fi
+if [ -z $CESM_BUILD_PATH ]; then
+    CESM_BUILD_PATH=$(pwd)
+fi
+mkdir -vp $CESM_BUILD_PATH
+echo "Build artifacts will be saved to $CESM_BUILD_PATH"
 
-outpath="$SCRATCH/$USER/XLM_build/$machine"
-mkdir -vp $outpath
-echo "Build artifacts will be saved to $outpath"
-#rm -rf $outpath/bld $outpath/run
-cmake -S src -B $outpath/bld -DCMAKE_C_COMPILER=mpicc \
+cmake -S src -B $CESM_BUILD_PATH/bld -DCMAKE_C_COMPILER=mpicc \
                     -DCMAKE_Fortran_COMPILER=mpif90 \
-                    -DCMAKE_INSTALL_PREFIX=$outpath/run \
+                    -DCMAKE_INSTALL_PREFIX=$CESM_BUILD_PATH/run \
                     -DCMAKE_MODULE_PATH=$(pwd)/cmake #-LA --trace
-cmake --build $outpath/bld #--clean-first #--parallel --verbose
-cmake --install $outpath/bld
-
-
-# === Dependency graph generation ===
-# dot requires graphviz package. 
-# On Ubuntu, run `sudo apt-get install graphviz`
-#cd bld
-#cmake --graphviz=graphfile.dot .
-#dot -T svg -o ~/cesm_depgraph.svg graphfile.dot 
+time cmake --build $CESM_BUILD_PATH/bld #--clean-first #--parallel --verbose
+cd $CESM_BUILD_PATH/bld
+make install
