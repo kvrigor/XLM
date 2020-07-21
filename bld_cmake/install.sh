1c51ba96b3d436259850771bd6dc892b3b272b9e
kvrigor <kvrigor@users.noreply.github.com>
2020-07-21 14:49:17 +0200

    Set build outputs to SCRATCH file system

--- bld_cmake/install.sh
+++ bld_cmake/install.sh
@@ -6,14 +6,16 @@ if [ ! -d src ]; then
     ./get_src_files.sh
 fi
 
-rm -rf bld run
-cmake -S src -B bld -DCMAKE_C_COMPILER=mpicc \
+outpath="$SCRATCH/$USER/XLM_build/$machine"
+mkdir -vp $outpath
+echo "Build artifacts will be saved to $outpath"
+#rm -rf $outpath/bld $outpath/run
+cmake -S src -B $outpath/bld -DCMAKE_C_COMPILER=mpicc \
                     -DCMAKE_Fortran_COMPILER=mpif90 \
-                    -DCMAKE_INSTALL_PREFIX=$(pwd)/run \
+                    -DCMAKE_INSTALL_PREFIX=$outpath/run \
                     -DCMAKE_MODULE_PATH=$(pwd)/cmake #-LA --trace
-cmake --build bld --clean-first #--parallel --verbose
-#cmake --build bld --target csm_share --clean-first
-cmake --install bld
+cmake --build $outpath/bld #--clean-first #--parallel --verbose
+cmake --install $outpath/bld
 
 
 # === Dependency graph generation ===
